8276e78bf29107c863a65e5330c52fc873e69c87
Eunchong C. Kim <chariskimec@gmail.com>
2019-06-17 12:23:21 +0900

    Modified text of notify

--- app.py
+++ app.py
@@ -89,7 +89,8 @@ def response_message(event):
     headers = {"Authorization" : "Bearer "+ LINENOTIFY_TOKEN}
 
     profile = line_bot_api.get_profile(event.source.user_id)
-    text = ("From: " + profile.display_name + "\n"
+    text = ("\n"
+            + "From: " + profile.display_name + "\n"
             + "userId: " + profile.user_id + "\n"
             + "message: " + event.message.text)
     payload = {"message" :  text}
